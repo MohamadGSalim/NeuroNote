35b497eb108435dcc9dcc853ffed4dccb2e7d703
Shadi <shadosite@gmail.com>
2023-04-06 17:04:22 -0400

    Backend setup

---
+++ backend/src/main.ts
@@ -0,0 +1,19 @@
+import 'reflect-metadata';
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Neuron Note API')
+    .setDescription('SOEN 357 UI Project')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(7070);
+}
+bootstrap();
