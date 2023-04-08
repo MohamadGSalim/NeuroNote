e6b602118e315841eda5915f0b88512c4ee6cf66
Arsollo <arsollo99@yahoo.com>
2023-04-08 18:57:06 -0400

    added info button to profile

--- client2/src/pages/profile.tsx
+++ client2/src/pages/profile.tsx
@@ -7,10 +7,23 @@ import { Avatar, AvatarBadge, AvatarGroup } from '@chakra-ui/react'
 import { Button, ButtonGroup } from '@chakra-ui/react'
 import { Image } from '@chakra-ui/react'
 import { Progress } from '@chakra-ui/react'
+import { InfoOutlineIcon } from '@chakra-ui/icons'
+import {
+    Popover,
+    PopoverTrigger,
+    PopoverContent,
+    PopoverHeader,
+    PopoverBody,
+    PopoverFooter,
+    PopoverArrow,
+    PopoverCloseButton,
+    PopoverAnchor,
+  } from '@chakra-ui/react'
 import "../styles/profile.css"
 
 
 function Profile() {
+
     return (
     <>
         <Nav />
@@ -22,12 +35,42 @@ function Profile() {
         
         <SimpleGrid columns={2} spacing={8}>
             <Box>
-                <Center>
-                <Avatar
-                    size={"3xl"}
-                    src={"https://avatars.dicebear.com/api/male/username.svg"}
-                />
-                </Center>  
+                <SimpleGrid columns={1} spacing={8}>
+                    <Box>
+                        <Center>
+                            <Avatar
+                                size={"3xl"}
+                                src={"https://avatars.dicebear.com/api/male/username.svg"}
+                            />
+                        </Center>
+                    </Box>
+
+                    <Box>
+                        <Center>
+                            <Heading as="h4">Time Traveler</Heading>
+                            <br></br>
+                            <div>
+                                <Popover>
+                                    <PopoverTrigger>
+                                        <Button><InfoOutlineIcon></InfoOutlineIcon></Button>
+                                    </PopoverTrigger>
+                                    <PopoverContent>
+                                        <PopoverArrow />
+                                        <PopoverCloseButton />
+                                        <PopoverHeader>EuroNote Level!</PopoverHeader>
+                                        <PopoverBody>The more you use Euronote, the closer you get to unlocking new rewards! 
+                                            <br></br>- Bronze Badge {'>'} 1 hour: Time Traveler
+                                            <br></br>- Silver Badge {'>'} 2 hours: Sound Seeker
+                                            <br></br>- Gold Badge {'>'} 4 hours: Music Maverick
+                                            <br></br>- Diamond Badge {'>'} 8 hours: Melody Master
+                                            <br></br>- Platinum Badge {'>'} 20 hours: Harmony Hero
+                                        </PopoverBody>
+                                    </PopoverContent>
+                                </Popover>
+                            </div>
+                        </Center>
+                    </Box>  
+                </SimpleGrid>
             </Box>
 
             <Box>
@@ -38,7 +81,6 @@ function Profile() {
                 <Center><Image src="bronze.png" id="profile_element"></Image></Center>
                 <div id="profile_progress"><Progress value={80} /></div>
                 <Center><div id="profile_progress_description">75/120 minutes until Silver level! </div></Center>
-                
             </Box>
 
             
@@ -55,7 +97,6 @@ function Profile() {
         </Center>
         
        
-      
 
     </>
     )
