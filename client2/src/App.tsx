a47cd0932de240ab2d7eb94f736f11c2d2d23fd4
Ahed Memas <ahed.mimas@gmail.com>
2023-04-08 19:04:24 -0400

    added Diary page

--- client2/src/App.tsx
+++ client2/src/App.tsx
@@ -6,6 +6,9 @@ import Register from "./pages/register";
 import Login from "./pages/login";
 import { ChakraProvider } from "@chakra-ui/react";
 import Home from "./pages/home";
+import Diary from "./pages/diary";
+import Feedback from "./pages/feedback";
+
 function App() {
   return (
     <ChakraProvider>
@@ -14,6 +17,9 @@ function App() {
           <Route path="/login" Component={Login} />
           <Route path="/register" Component={Register} />
           <Route path="/" Component={Home} />
+          <Route path="/diary" Component={Diary} />
+          <Route path="/feedback" Component={Feedback} />
+
         </Routes>
       </Router>
     </ChakraProvider>
---
+++ client2/src/pages/feedback.tsx
@@ -0,0 +1,185 @@
+import React from 'react';
+import { useState } from 'react';
+import {
+Box,
+Heading,
+Button,
+FormControl,
+FormLabel,
+Radio,
+RadioGroup,
+useToast,
+} from '@chakra-ui/react';
+import Nav from '../components/NavBar';
+
+export default function Feedback() {
+   const [visualizations, setVisualizations] = useState('');
+   const [features, setFeatures] = useState('');
+   const [distracting, setDistracting] = useState('');
+   const [design, setDesign] = useState('');
+   const [accuracy, setAccuracy] = useState('');
+   const [feedback, setFeedback] = useState('');
+   const [, setSubmitted] = useState(false);
+
+const toast = useToast();
+
+const handleSubmit = (e) => {
+   e.preventDefault();
+   setSubmitted(true);
+toast({
+   title: 'Feedback submitted',
+   description: 'Thank you for your feedback!',
+   status: 'success',
+   duration: 5000,
+   isClosable: true,
+});
+setVisualizations('');
+setFeatures('');
+setDistracting('');
+setDesign('');
+setAccuracy('');
+setFeedback('');
+};
+
+return (
+  <Box>  
+         <title>NeuroNote Feedback</title>
+         <meta name="description" content="Feedback page for NeuroNote" />
+         <meta name="viewport" content="width=device-width, initial-scale=1" />
+      
+      <Nav />
+      <Box p="4" bg="gray.100" textAlign="center">
+         <Box bg="white" w="100%" maxWidth="50rem" mx="auto" p="4">
+         <Heading as="h1" mb="4">
+            Feedback
+         </Heading>
+         <Box as="p" mb="4">
+            Answer the following questions to provide feedback on your experience with NeuroNote.
+         </Box>
+         <form onSubmit={handleSubmit}>
+            <FormControl mb="8" isRequired>
+               <FormLabel as="h2" fontSize="lg" fontWeight="bold">
+                  1) Do you find the graphs and visualizations in the app distracting?
+               </FormLabel>
+               <RadioGroup
+               name="visualizations"
+               onChange={(value) => setVisualizations(value)}
+               value={visualizations}
+               >
+                  <Radio value="Yes, they are too distracting">Yes, they are too distracting</Radio>
+                  <Radio value="No, they are helpful in understanding the data">No, they are helpful in understanding the data</Radio>
+                  <Radio value="I don't have an opinion">I don't have an opinion</Radio>
+               </RadioGroup>
+            </FormControl>
+            <FormControl mb="8" isRequired>
+               <FormLabel as="h2" fontSize="lg" fontWeight="bold">
+                  2) Do you feel that the app's features are organized in a logical and intuitive manner?
+               </FormLabel>
+               <RadioGroup
+               name="features"
+               onChange={(value) => setFeatures(value)}
+               value={features}
+               >
+                  <Radio value="Yes, they are easy to find and use">
+                     Yes, they are easy to find and use
+                  </Radio>
+                  <Radio value="No, they are difficult to find and use">
+                     No, they are difficult to find and use
+                  </Radio>
+                  <Radio value="Unclear and difficult to navigate">
+                     Unclear and difficult to navigate
+                  </Radio>
+               </RadioGroup>
+            </FormControl>
+            <FormControl mb="8" isRequired>
+               <FormLabel as="h2" fontSize="lg" fontWeight="bold">
+                  3) Which of the following features would make it easier for you to use the app?
+               </FormLabel>
+               <RadioGroup
+               name="distracting"
+               onChange={(value) => setDistracting(value)}
+               value={distracting}
+               >
+                  <Radio value="More detailed explanations and tutorials">
+                     More detailed explanations and tutorials
+                  </Radio>
+                  <Radio value="Fewer graphs and visualizations">
+                     Fewer graphs and visualizations
+                  </Radio>
+                  <Radio value="More customization options">
+                     More customization options
+                  </Radio>
+                  <Radio value="Other">Other</Radio>
+               </RadioGroup>
+            </FormControl>
+            <FormControl mb="8" isRequired>
+               <FormLabel as="h2" fontSize="lg" fontWeight="bold">
+                  4) How do you feel about the overall design and visual appeal of NeuroNote?
+               </FormLabel>
+               <RadioGroup
+               name="design"
+               onChange={(value) => setDesign(value)}
+               value={design}
+               >
+                  <Radio value="Very visually appealing">Very visually appealing</Radio>
+                  <Radio value="Somewhat visually appealing">
+                     Somewhat visually appealing
+                  </Radio>
+                  <Radio value="Not very visually appealing">
+                     Not very visually appealing
+                  </Radio>
+                  <Radio value="Not visually appealing at all">
+                     Not visually appealing at all
+                  </Radio>
+               </RadioGroup>
+            </FormControl>
+            <FormControl mb="8" isRequired>
+               <FormLabel as="h2" fontSize="lg" fontWeight="bold">
+                  5) How do you feel about the accuracy of the EEG headset in detecting your brainwaves and translating them into music notes?
+               </FormLabel>
+               <RadioGroup
+               name="accuracy"
+               onChange={(value) => setAccuracy(value)}
+               value={accuracy}
+               >
+                  <Radio value="It is very accurate">It is very accurate</Radio>
+                  <Radio value="It is somewhat accurate">
+                     It is somewhat accurate
+                  </Radio>
+                  <Radio value="It is not accurate">It is not accurate</Radio>
+               </RadioGroup>
+            </FormControl>
+            <FormControl mb="8" isRequired>
+               <FormLabel as="h2" fontSize="lg" fontWeight="bold">
+                  6) Do you feel that NeuroNote provides enough feedback and guidance to help you improve your brainwave control and create better music?
+               </FormLabel>
+               <RadioGroup
+               name="feedback"
+               onChange={(value) => setFeedback(value)}
+               value={feedback}
+               >
+                  <Radio value="Yes, the feedback and guidance are helpful"> Yes, the feedback and guidance are helpful</Radio>
+                  <Radio value="Somewhat, but more feedback and guidance would be helpful"> 
+                     Somewhat, but more feedback and guidance would be helpful
+                  </Radio>
+                  <Radio value="No, the feedback and guidance are not helpful">
+                     No, the feedback and guidance are not helpful
+                  </Radio>
+               </RadioGroup>
+            </FormControl>
+            <Button
+            type="submit"
+            colorScheme="blue"
+            size="lg"
+            w="100%"
+            maxWidth="20rem"
+            mx="auto"
+            >
+               Submit
+            </Button>
+         </form>
+         </Box>
+      </Box>
+   </Box>
+   );
+}
