e9b3a4fdd59bcabe752c4fe3d3f8b25a2d9ad04f
rogersi9 <69977233+rogersi9@users.noreply.github.com>
2023-04-12 18:09:59 -0400

    Fixed profile page to make it centered

--- client2/src/pages/profile.tsx
+++ client2/src/pages/profile.tsx
@@ -1,65 +1,66 @@
-import Nav from '../components/NavBar'
-import { useState } from 'react'
-import { Center, Square, Circle, useTagStyles } from '@chakra-ui/react'
-import { Heading } from '@chakra-ui/react'
-import { SimpleGrid } from '@chakra-ui/react'
-import { Box } from '@chakra-ui/react'
-import { Avatar, AvatarBadge, AvatarGroup } from '@chakra-ui/react'
-import { Button, ButtonGroup } from '@chakra-ui/react'
-import { Image } from '@chakra-ui/react'
-import { Progress } from '@chakra-ui/react'
-import { InfoOutlineIcon } from '@chakra-ui/icons'
+import Nav from "../components/NavBar";
+import { useState } from "react";
 import {
-    Popover,
-    PopoverTrigger,
-    PopoverContent,
-    PopoverHeader,
-    PopoverBody,
-    PopoverFooter,
-    PopoverArrow,
-    PopoverCloseButton,
-    PopoverAnchor,
-  } from '@chakra-ui/react'
-import "../styles/profile.css"
-
+  Center,
+  Square,
+  Circle,
+  useTagStyles,
+  AbsoluteCenter,
+} from "@chakra-ui/react";
+import { Heading } from "@chakra-ui/react";
+import { SimpleGrid } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
+import { Avatar, AvatarBadge, AvatarGroup } from "@chakra-ui/react";
+import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Image } from "@chakra-ui/react";
+import { Progress } from "@chakra-ui/react";
+import { InfoOutlineIcon } from "@chakra-ui/icons";
+import {
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  PopoverHeader,
+  PopoverBody,
+  PopoverFooter,
+  PopoverArrow,
+  PopoverCloseButton,
+  PopoverAnchor,
+  Text,
+} from "@chakra-ui/react";
+import "../styles/profile.css";
 
 function Profile() {
+  const [name, setName] = useState("Name");
+  const [profilePic, setProfilePic] = useState(
+    "https://avatars.dicebear.com/api/male/username.svg"
+  );
+  const [userName, setUserName] = useState("UserName");
+  const [email, setEmail] = useState("user@gmail.com");
+  const [level, setLevel] = useState("Time Traveler");
+  const [nextLevel, setNextLevel] = useState("Silver Badge");
+  const [xp, setXP] = useState("74/120");
+  const [badge, setBadge] = useState("bronze.png");
 
-    const [name, setName] = useState("Name")
-    const [profilePic, setProfilePic] = useState("https://avatars.dicebear.com/api/male/username.svg")
-    const [userName, setUserName] = useState("UserName")
-    const [email, setEmail] = useState("user@gmail.com")
-    const [level, setLevel] = useState("Time Traveler")
-    const [nextLevel, setNextLevel] = useState("Silver Badge")
-    const [xp, setXP] = useState("74/120")
-    const [badge, setBadge] = useState("bronze.png")
-
-    return (
+  return (
     <>
-        <Nav />
-        <br></br>
-
+      <Nav />
+      <Box overflow="hidden" p="8">
         <Center>
-            <Heading as='h1' size='3xl' className="profile_title">Profile</Heading>
+          <Avatar size="2xl" name="Roger Sioufi" src={profilePic} border='1px' borderColor='gray.200'>
+        </Avatar>
         </Center>
-        
-        <SimpleGrid columns={2} spacing={8}>
-            <Box>
-                <SimpleGrid columns={1} spacing={8}>
-                    <Box>
+        <Box textAlign="center" mt="4">
+          <Heading as="h2" size="lg">
+            Roger Sioufi
+          </Heading>
+          <Text fontWeight="bold" mt="2">
+            john.doe@example.com
+          </Text>
+          <Box alignItems="center" mt="12">
+          <Box>
                         <Center>
-                            <Avatar
-                                size={"3xl"}
-                                src={profilePic}
-                            />
-                        </Center>
-                    </Box>
-
-                    <Box>
-                        <Center>
-                            <Heading as="h4">{level}</Heading>
+                            <Heading as="h4" mr="4">Level 1: Time Traveller</Heading>
                             <br></br>
-                            <div>
                                 <Popover>
                                     <PopoverTrigger>
                                         <Button><InfoOutlineIcon></InfoOutlineIcon></Button>
@@ -67,8 +68,8 @@ function Profile() {
                                     <PopoverContent>
                                         <PopoverArrow />
                                         <PopoverCloseButton />
-                                        <PopoverHeader>EuroNote Level!</PopoverHeader>
-                                        <PopoverBody>The more you use Euronote, the closer you get to unlocking new rewards! 
+                                        <PopoverHeader>NeuroNote Level!</PopoverHeader>
+                                        <PopoverBody>The more you use NeurNnote, the closer you get to unlocking new rewards! 
                                             <br></br>- Bronze Badge {'>'} 1 hour: Time Traveler
                                             <br></br>- Silver Badge {'>'} 2 hours: Sound Seeker
                                             <br></br>- Gold Badge {'>'} 4 hours: Music Maverick
@@ -77,48 +78,25 @@ function Profile() {
                                         </PopoverBody>
                                     </PopoverContent>
                                 </Popover>
-                            </div>
                         </Center>
-                    </Box>  
-                </SimpleGrid>
+                    </Box> 
+            <Box display="flex" w="100%" position='relative' justifyContent="space-between">
+                <Box width="10%">
+                    <Image src='/bronze.png' alt='Bronze Level' width="100%" height='auto'/>
+                    <Text><b>Level 1:</b> <br /> Time Traveller</Text>
+                </Box>
+              <AbsoluteCenter axis="both" width="70%">
+                <Progress value={80} width="100%" />
+              </AbsoluteCenter>
+              <Box width="10%">
+                <Image src='/silver.png' alt='Siver Level' width="100%" height='auto'/>
+                <Text> <b>Level 2:</b>  <br /> Sound Seeker</Text>
+                </Box>
             </Box>
-
-            <Box>
-                <Heading as='h3' id="profile_element">{name}</Heading>
-                <Heading as='h3' id="profile_element">{userName}</Heading>
-                <Heading as='h3' id="profile_element">{email}</Heading>
-                <Center><Image src={badge} id="profile_element"></Image></Center>
-                <div id="profile_progress"><Progress value={80} /></div>
-                <Center><div id="profile_progress_description">{xp} minutes until {nextLevel}! </div></Center>
-            </Box>
-
-            
-        </SimpleGrid>
-        <Center>
-            <Button id="profile_log_out"
-                size="lg"
-                loadingText='Loggin Out...'
-                colorScheme='teal'
-                variant='outline'
-            >
-                Edit Profile!
-            </Button>
-
-            <br></br>
-            <br></br>
-
-            <Button id="profile_log_out"
-                size="lg"
-                loadingText='Loggin Out...'
-                colorScheme='teal'
-                variant='outline'
-            >
-                Log out!
-            </Button>
-        </Center>
-        
-    
+          </Box>
+        </Box>
+      </Box>
     </>
-    )
+  );
 }
 export default Profile;
