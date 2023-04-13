5ac3ec1745ed6795f6e4736930279a5dd9e9dbf9
Shadi <shadosite@gmail.com>
2023-04-13 14:08:10 -0400

    Removed Nav from login and register

--- client2/src/pages/login.tsx
+++ client2/src/pages/login.tsx
@@ -19,7 +19,6 @@ export default function LoginPage() {
 
 	return (
 		<div>
-			<Nav />
 			<LoginComponent />
 		</div>
 	);
--- client2/src/pages/register.tsx
+++ client2/src/pages/register.tsx
@@ -19,7 +19,6 @@ export default function RegisterPage() {
 
 	return (
 		<div>
-			<Nav />
 			<RegisterComponent />
 		</div>
 	);
